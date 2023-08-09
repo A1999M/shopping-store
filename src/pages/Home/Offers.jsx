bff99ffca93b2f32068750f90706ea11b65cc5db
DESKTOP-T2KVN3S\Mr_Developer <molaei_Developer>
2023-08-09 21:56:47 +0430

    responsive offer of home page

--- src/pages/Home/Offers.jsx
+++ src/pages/Home/Offers.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import OfferItems from "./OfferItems";
 
 export default function Offers() {
   let scopeRef = useRef();
@@ -9,26 +10,10 @@ export default function Offers() {
   let titleRef = useRef();
   let descRef = useRef();
 
-  let titleLeftOffer = useRef();
-  let descLeftOffer = useRef();
-  let btnLeftOffer = useRef();
-
-  let titleLRightOffer = useRef();
-  let descLRightOffer = useRef();
-  let btnLRightOffer = useRef();
-
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.refresh();
     let ctx = gsap.context(() => {
-      gsap.set(leftOfferRef.current, {
-        clipPath: "inset(100% 0% 0% 0%)",
-        backgroundSize: "125%",
-      });
-      gsap.set(rightOfferRef.current, {
-        clipPath: "inset(100% 0% 0% 0%)",
-        backgroundSize: "125%",
-      });
       gsap.set(titleRef.current, {
         opacity: 0,
         clipPath: "inset(0% 0% 100% 0%)",
@@ -67,102 +52,6 @@ export default function Offers() {
         },
         "<0.3"
       );
-
-      let tl1 = gsap.timeline({
-        scrollTrigger: {
-          trigger: leftOfferRef.current,
-          start: "center 85%",
-          end: "bottom 0%",
-          id: "trigger1",
-        },
-      });
-      let tl2 = gsap.timeline({
-        scrollTrigger: {
-          trigger: rightOfferRef.current,
-          start: "center 85%",
-          end: "bottom 0%",
-          id: "trigger2",
-        },
-      });
-
-      tl1.to(leftOfferRef.current, {
-        clipPath: "inset(0% 0% 0% 0%)",
-        backgroundSize: "100%",
-        duration: 2,
-        ease: "Expo.easeOut",
-      });
-      tl1.to(
-        titleLeftOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          y: 0,
-          duration: 1,
-          ease: "Expo.easeOut",
-        },
-        "<0.4"
-      );
-      tl1.to(
-        descLeftOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          y: 0,
-          duration: 1,
-          ease: "Expo.easeOut",
-        },
-        "<0.1"
-      );
-      tl1.to(
-        btnLeftOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          duration: 0.8,
-          ease: "Expo.easeOut",
-        },
-        "<0.25"
-      );
-
-      tl2.to(rightOfferRef.current, {
-        clipPath: "inset(0% 0% 0% 0%)",
-        backgroundSize: "100%",
-        duration: 2,
-        ease: "Expo.easeOut",
-        delay: 0.4,
-      });
-      tl2.to(
-        titleLRightOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          y: 0,
-          duration: 1,
-          ease: "Expo.easeOut",
-        },
-        "<0.4"
-      );
-      tl2.to(
-        descLRightOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          y: 0,
-          duration: 1,
-          ease: "Expo.easeOut",
-        },
-        "<0.1"
-      );
-      tl2.to(
-        btnLRightOffer.current,
-        {
-          opacity: 1,
-          clipPath: "inset(0% 0% 0% 0%)",
-          duration: 0.8,
-          ease: "Expo.easeOut",
-        },
-        "<0.25"
-      );
     }, scopeRef);
 
     return () => {
@@ -170,35 +59,6 @@ export default function Offers() {
       ScrollTrigger.refresh();
     };
   }, []);
-
-  let handlerLImageHoverEnter = () => {
-    gsap.to(document.querySelector(".wrapperLeftOffer"), {
-      backgroundSize: "115%",
-      duration: 1,
-      ease: "Expo.easeOut",
-    });
-  };
-  let handlerLImageHoverLeave = () => {
-    gsap.to(document.querySelector(".wrapperLeftOffer"), {
-      backgroundSize: "100%",
-      duration: 1,
-      ease: "Expo.easeOut",
-    });
-  };
-  let handlerRImageHoverEnter = () => {
-    gsap.to(document.querySelector(".wrapperRightOffer"), {
-      backgroundSize: "115%",
-      duration: 1,
-      ease: "Expo.easeOut",
-    });
-  };
-  let handlerRImageHoverLeave = () => {
-    gsap.to(document.querySelector(".wrapperRightOffer"), {
-      backgroundSize: "100%",
-      duration: 1,
-      ease: "Expo.easeOut",
-    });
-  };
 
   return (
     <>
@@ -213,42 +73,22 @@ export default function Offers() {
             </p>
           </div>
         </div>
-        <div className="row mt-3rem">
-          <div className="col-6">
-            <div
-              onMouseEnter={handlerLImageHoverEnter}
-              onMouseLeave={handlerLImageHoverLeave}
-              className="wrapperLeftOffer"
-              ref={leftOfferRef}
-            >
-              <p ref={titleLeftOffer} className="titleLeftOffer">
-                MEN’S WEAR
-              </p>
-              <p ref={descLeftOffer} className="descLeftOffer">
-                Big Sale 50% Off
-              </p>
-              <button ref={btnLeftOffer} className="shopLeftOffer">
-                shop now
-              </button>
-            </div>
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <OfferItems
+              title="MEN’S WEAR"
+              desc="Big Sale 50% Off"
+              index="1"
+              bgImge="https://pandora-fashi.myshopify.com/cdn/shop/files/offer-bg-1.png?v=1681792772&width=750"
+            />
           </div>
-          <div className="col-6">
-            <div
-              onMouseEnter={handlerRImageHoverEnter}
-              onMouseLeave={handlerRImageHoverLeave}
-              className="wrapperRightOffer"
-              ref={rightOfferRef}
-            >
-              <p ref={titleLRightOffer} className="titleRightOffer">
-                FLASH SALE
-              </p>
-              <p ref={descLRightOffer} className="descRightOffer">
-                Upto 60% Discount
-              </p>
-              <button ref={btnLRightOffer} className="shopRightOffer">
-                shop now
-              </button>
-            </div>
+          <div className="col-12 col-md-6">
+            <OfferItems
+              title="FLASH SALE"
+              desc="Upto 60% Discount"
+              index="2"
+              bgImge="https://pandora-fashi.myshopify.com/cdn/shop/files/offer-bg-2.png?v=1681792780&width=750"
+            />
           </div>
         </div>
       </div>
