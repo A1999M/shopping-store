d591851ee0b9af3987589429f83788884f72eabe
DESKTOP-T2KVN3S\Mr_Developer <molaei_Developer>
2023-08-12 16:20:46 +0430

    responsive cart items of shopping cart page

--- src/pages/ShoppingCart/CartItems.jsx
+++ src/pages/ShoppingCart/CartItems.jsx
@@ -5,7 +5,7 @@ import { cartActions } from "../../store/cartSlice";
 import { useLayoutEffect } from "react";
 import "./ShoppingCart.scss";
 
-export default function CartItems({ item }) {
+export default function CartItems({ item, size }) {
   let dispatch = useDispatch();
   useLayoutEffect(() => {
     let elms = document.querySelectorAll(".cartItems");
@@ -28,9 +28,9 @@ export default function CartItems({ item }) {
       <motion.div
         initial="false"
         exit={{ opacity: 0, x: -100 }}
-        className="row px-5 cartItems"
+        className="row cartItems"
       >
-        <div className="col-5">
+        <div className="col-9 col-sm-5">
           <div className="wrapperCartDetails">
             <div className="wrapperCartImg">
               <img src={item.imageSrc} alt={item.name} />
@@ -41,49 +41,89 @@ export default function CartItems({ item }) {
                 <span className="shippingDiscuont">35% shipping discount</span>
                 <span className="merchantDiscuont">25% merchant discount</span>
               </div>
+              {size <= 576 && (
+                <div className="wrapperQuantityBtns">
+                  <div class="cartCountBtns">
+                    <span
+                      onClick={() => dispatch(cartActions.increment(item.id))}
+                      class="increment"
+                    >
+                      +
+                    </span>
+                    <span class="choosenProCount">{item.count}</span>
+                    <span
+                      onClick={() => {
+                        if (item.count >= 2) {
+                          dispatch(cartActions.decrement(item.id));
+                        }
+                      }}
+                      style={
+                        item.count == 1
+                          ? { cursor: "no-drop" }
+                          : { cursor: "pointer" }
+                      }
+                      class="decrement"
+                    >
+                      −
+                    </span>
+                  </div>
+                  <span
+                    onClick={() => {
+                      dispatch(cartActions.removeCartItems(item.id));
+                    }}
+                    class="material-symbols-outlined removeBtn"
+                  >
+                    delete
+                  </span>
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <div className="col-2">
-          <p className="cartItemPrice">${item.price}.00</p>
-        </div>
-        <div className="col-3">
-          <div className="wrapperQuantityBtns">
-            <div class="cartCountBtns">
-              <span
-                onClick={() => dispatch(cartActions.increment(item.id))}
-                class="increment"
-              >
-                +
-              </span>
-              <span class="choosenProCount">{item.count}</span>
+        {size >= 768 && (
+          <div className="col-1 col-sm-2">
+            <p className="cartItemPrice">${item.price}.00</p>
+          </div>
+        )}
+        {size > 576 && (
+          <div className="col-4 col-md-3">
+            <div className="wrapperQuantityBtns">
+              <div class="cartCountBtns">
+                <span
+                  onClick={() => dispatch(cartActions.increment(item.id))}
+                  class="increment"
+                >
+                  +
+                </span>
+                <span class="choosenProCount">{item.count}</span>
+                <span
+                  onClick={() => {
+                    if (item.count >= 2) {
+                      dispatch(cartActions.decrement(item.id));
+                    }
+                  }}
+                  style={
+                    item.count == 1
+                      ? { cursor: "no-drop" }
+                      : { cursor: "pointer" }
+                  }
+                  class="decrement"
+                >
+                  −
+                </span>
+              </div>
               <span
                 onClick={() => {
-                  if (item.count >= 2) {
-                    dispatch(cartActions.decrement(item.id));
-                  }
+                  dispatch(cartActions.removeCartItems(item.id));
                 }}
-                style={
-                  item.count == 1
-                    ? { cursor: "no-drop" }
-                    : { cursor: "pointer" }
-                }
-                class="decrement"
+                class="material-symbols-outlined removeBtn"
               >
-                −
+                delete
               </span>
             </div>
-            <span
-              onClick={() => {
-                dispatch(cartActions.removeCartItems(item.id));
-              }}
-              class="material-symbols-outlined removeBtn"
-            >
-              delete
-            </span>
           </div>
-        </div>
-        <div className="col-2">
+        )}
+        <div className="col-3 col-sm-2">
           <p className="totalPriceCart">${item.totalPrice}.00</p>
         </div>
       </motion.div>
